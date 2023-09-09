1e91c5faf3e3cf53c1623c5f514f5391db25b124
mrdnewman <misterdnewman@icloud.com>
2023-09-09 01:57:00 -0500

    Update my_test_script.py

--- my_test_script.py
+++ my_test_script.py
@@ -1,7 +1,5 @@
 import time
 
-time.sleep(10)
-
 def add(a, b):
     """Add two numbers and return the result."""
     return a + b
@@ -10,6 +8,7 @@ def test_addition():
     """Test the addition function."""
     result = add(2, 3)
     assert result == 5, f"Expected 5 but got {result}"
+    time.sleep(10)
 
 if __name__ == "__main__":
     test_addition()
