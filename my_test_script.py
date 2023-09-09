dec4d109f055dcf41733e4fcfce96c5391b2e1a8
mrdnewman <misterdnewman@icloud.com>
2023-09-09 12:06:43 -0500

    Update my_test_script.py

--- my_test_script.py
+++ my_test_script.py
@@ -8,16 +8,23 @@ def test_addition():
     """Test the addition function."""
     result = add(2, 3)
     assert result == 5, f"Expected 5 but got {result}"
-    print("Starting 10 Sec Count Down...")
 
-    Number = 1
-    while True:
-        if Number > 10:
-            break
 
-        print(Number)
-        Number += 1
-        time.sleep(1)
+while countdown > 0:
+    # Display the countdown number
+    print(countdown)
+    
+    # Pause for 1 second
+    time.sleep(1)
+    
+    # Clear the screen (works on Unix-like systems, might not work on all platforms)
+    os.system('clear')  # For Windows, you can use 'cls' instead of 'clear'
+    
+    # Decrement the countdown
+    countdown -= 1
+
+# Clear the screen one last time
+os.system('clear')
 
 if __name__ == "__main__":
     test_addition()
